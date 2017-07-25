9972475e1292597aa2b6b53c1460f8374e8fdefa
MarySalemme <marysalemme1@gmail.com>
2017-07-25 10:38:50 +0100

    Adding validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
-
+    @article = Article.new
   end
 
   def create
@@ -19,10 +19,14 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
 
     # the below is responsible for saving the model in the database
-    @article.save
-
-    # redirect the user to the show action
-    redirect_to @article
+    if @article.save
+      # redirect the user to the show action
+      redirect_to @article
+    else
+      # we use render and not redirect so that the @article object is passed back to the new template
+      #  when it's rendered, whereas 'redirect' tells the browser to issue another request
+      render 'new'
+    end
   end
 
   # Whitelisting controller parameters to prevent wrong mass assignment
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
-# bin/rails generate model article title:string text:text
+# created with: $bin/rails generate model article title:string text:text
 
 class Article < ApplicationRecord
+  # our class Article inherits data validation from ApplicationRecord, which inherits from ActiveRecord
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+  # this will ensure that all articles have a title that is at least five characters long
 end
